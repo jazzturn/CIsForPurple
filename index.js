5e91164bfa705f0b9230d639aee44e7ea2e08d61
Jasmine Turner <jazzturn@gmail.com>
2018-02-15 00:15:05 -0600

    commented pseudocode in js file

---
+++ index.js
@@ -0,0 +1,13 @@
+//points should equal 0
+//game to start if space bar is pressed, space also pauses game
+//ENTER resets game
+//a note C4 is played as a reference when the space bar is pressed and wait one second after c note is played
+//randomize all notes C4-C5 in an array, starting with one, and 2 secs before the next
+//if when a C4 or C5 play, and the purple button is clicked, then increase score by 1
+    //if C4 or C5 play and the red button is clicked, the decrease score by 1
+
+//if when all other notes are playing and red button is clicked, the increase score by 1
+    //if all other notes are playing and purple button is clicked, the decrease score by 1
+
+//if -10 is reached, switch happy face, to sad face, and reset the game back to smiley face and a score of 0
+//if 10 is reached, stay at happy face, give congrats, and reset the game back to 0
